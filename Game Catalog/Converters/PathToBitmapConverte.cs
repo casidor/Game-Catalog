2b195821e52e9df70b6e0c37d07b33d747f27b51
Roman Voloshyn <168105827+casidor@users.noreply.github.com>
2026-05-10 16:31:42 +0300

    feat: replace library list with cover cards grid

---
+++ Game Catalog/Converters/PathToBitmapConverte.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Game_Catalog.Converters
+{
+    /// <summary> Converts a file path string to an Avalonia Bitmap for image binding. </summary>
+    public class PathToBitmapConverter : IValueConverter
+    {
+        public static readonly PathToBitmapConverter Instance = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is string path && File.Exists(path))
+                return new Bitmap(path);
+            return null;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
